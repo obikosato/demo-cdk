2bb3206ec79bc73c0307d6f881eecf6748a1d1d6
Obiko Sato <Obiko.Sato@jp.ricoh.com>
2022-10-05 05:04:28 +0900

    aa

--- lib/demo-cdk-stack.ts
+++ lib/demo-cdk-stack.ts
@@ -1,19 +1,15 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class DemoCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'DemoCdkQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler',
     });
-
-    const topic = new sns.Topic(this, 'DemoCdkTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
